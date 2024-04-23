826a4b544212677ad4d2e22124c955faa2c2a642
jpasholk <josh.pasholk+github@gmail.com>
2024-04-22 19:37:36 -0700

    First commit

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,11 @@
 import { defineConfig } from 'astro/config';
+import tailwind from "@astrojs/tailwind";
+import sitemap from "@astrojs/sitemap";
+import mdx from "@astrojs/mdx";
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  // TODO: Add URL here before deplying
+  // site: '',
+  integrations: [tailwind(), sitemap(), mdx()]
+});
---
+++ src/consts.ts
@@ -0,0 +1,10 @@
+// Site metadata
+
+export const SITE_META = {
+    title: 'Skyrim Alchemy Tool',
+    headerTitle: '',
+    desc: 'Find ingredients by searching for their effects',
+    siteURL: '',
+    siteREPO: '',
+    siteLogo: '',
+}

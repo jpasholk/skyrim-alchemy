4647742d7f081f515e60f7b1711fafa10460d6ac
jpasholk <josh.pasholk+github@gmail.com>
2024-04-22 19:47:08 -0700

    Update README

--- src/consts.ts
+++ src/consts.ts
@@ -3,7 +3,7 @@
 export const SITE_META = {
     title: 'Skyrim Alchemy Tool',
     headerTitle: '',
-    desc: 'Find ingredients by searching for their effects',
+    desc: 'Find ingredients by searching for their effects.',
     siteURL: '',
     siteREPO: '',
     siteLogo: '',

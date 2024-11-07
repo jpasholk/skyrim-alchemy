e1e4769ced36e562b3d3a9849adeba016f87ed2d
jpasholk <josh.pasholk+github@gmail.com>
2024-11-07 15:44:50 -0800

    Initial commit

---
+++ src/config.ts
@@ -0,0 +1,7 @@
+// Place any global data in this file.
+// You can import this data from anywhere in your site by using the `import` keyword.
+
+export const SITE_TITLE = 'My personal website.';
+export const SITE_DESCRIPTION = 'Welcome to my website!';
+
+// Remember to set your site URL in astro.config.mjs

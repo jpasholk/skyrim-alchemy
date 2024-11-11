993330da0e420e74d7f457aab3a5693c24870908
jpasholk <josh.pasholk+github@gmail.com>
2024-11-10 18:39:28 -0800

    Added an effect matcher, and made some small layout changes. Added site URL.

--- src/config.ts
+++ src/config.ts
@@ -2,6 +2,6 @@
 // You can import this data from anywhere in your site by using the `import` keyword.
 
 export const SITE_TITLE = 'Skyrim Alchemy Tool';
-export const SITE_DESCRIPTION = 'A tool to help you master alchemy in Skyrim.';
+export const SITE_DESCRIPTION = 'Master alchemy in Skyrim.';
 
 // Remember to set your site URL in astro.config.mjs
